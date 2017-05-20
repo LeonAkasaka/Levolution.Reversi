3620c57bd13fffb8ba1c74f0a87f2e693caf1a28
Leon Akasaka <leon@wisdomsoft.jp>
2017-05-20 19:43:53 +0900

    Add unity .meta files, change project settings.

---
+++ Levolution.Reversi.Games.Unity/Assets/Scripts/TableCellView.cs
@@ -0,0 +1,7 @@
+﻿/// <summary>
+/// Absorber for unity reference.
+/// </summary>
+/// <remarks>
+/// It had better use this class instead of <see cref="Levolution.Reversi.Components.TableCellView"/>.
+/// </remarks>
+public class TableCellView : Levolution.Reversi.Components.TableCellView { }
---
+++ Levolution.Reversi.Games.Unity/Assets/Scripts/TableView.cs
@@ -0,0 +1,7 @@
+﻿/// <summary>
+/// Absorber for unity reference.
+/// </summary>
+/// <remarks>
+/// It had better use this class instead of <see cref="Levolution.Reversi.Components.TableView"/>.
+/// </remarks>
+public class TableView : Levolution.Reversi.Components.TableView { }
